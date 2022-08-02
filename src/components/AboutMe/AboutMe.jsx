bab20b0274bb5c0e40c9d2c1e2c4d7c11963a791
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-08-02 12:03:04 -0300

    Background agregado

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Andres Gomez</h1>
+          <p>FullStack Developer</p>
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quae dolore libero, harum in nisi adipisci molestiae ullam saepe ratione, illo commodi sit delectus! Suscipit, aliquam corporis? Nemo, amet. Nemo!
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Design/Background.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./Background.css"
+
+const Background = () => {
+    return ( <Fragment>
+        <div className="horizontal"></div>
+        <div className="vertical"></div>
+        <div className="contenido"></div>
+    </Fragment> );
+}
+ 
+export default Background;
