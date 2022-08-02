14ce697d07c6c256a20189fb1d3446870abe3aaf
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-08-02 12:05:38 -0300

    Lorem eliminado

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,8 +9,7 @@ const AboutMe = () => {
           <p>FullStack Developer</p>
         </div>
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quae dolore libero, harum in nisi adipisci molestiae ullam saepe ratione, illo commodi sit delectus! Suscipit, aliquam corporis? Nemo, amet. Nemo!
-        </div>
+       </div>
       </div>
     </section>
   );
